dd60cae72e936657cdc17411c1fdf64d226061cf
ahamouda88 <ahamouda@ksu.edu>
2014-07-01 16:32:12 -0500

    Updating Spring Configuration

--- LibrarySystem-EJB/src/main/java/librarysys/springconfig/SpringBeansConfig.java
+++ LibrarySystem-EJB/src/main/java/librarysys/springconfig/SpringBeansConfig.java
@@ -3,27 +3,44 @@ package librarysys.springconfig;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @ComponentScan({"librarysys.entities","librarysys.spring"})
 @ImportResource("classpath:META-INF/spring-config.xml")
+@PropertySource("classpath:persistence-mysql.properties")
 public class SpringBeansConfig {
 
+// TODO Convert from XML Configuration to Java Configuration and Annotations
 	
-	@Bean
-	public DataSource dataSource() {
-		
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost/..");
-		dataSource.setUsername("username");
-		dataSource.setPassword("password");
-		return dataSource;
-	}
+//	@Value("${datbaase.url}")
+//	private String databaseURL;
+	
+//	@Bean
+//	public DataSource dataSource() {
+//		
+//		BasicDataSource dataSource = new BasicDataSource();
+//		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//		dataSource.setUrl("jdbc:mysql://localhost/..");
+//		dataSource.setUsername("username");
+//		dataSource.setPassword("password");
+//		return dataSource;
+//	}
+	
+//	@Bean
+//	public PropertyPlaceholderConfigurer getPropertyPlaceholderConfigurer() {
+//		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+//		ppc.setLocation(new ClassPathResource("application.properties"));
+//		ppc.setIgnoreUnresolvablePlaceholders(true);
+//		return ppc;
+//	}
 	
 //	@Bean(name = "Address")
 //	public Address address(){

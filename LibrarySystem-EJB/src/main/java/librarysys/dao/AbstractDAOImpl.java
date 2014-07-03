5685691eec450d9d4fc272668fe0ef05a40352b3
ahamouda88 <ahamouda@ksu.edu>
2014-07-03 16:29:30 -0500

    Adding abstract methods in AbstractDAOImpl

--- LibrarySystem-EJB/src/main/java/librarysys/dao/AbstractDAOImpl.java
+++ LibrarySystem-EJB/src/main/java/librarysys/dao/AbstractDAOImpl.java
@@ -1,18 +1,39 @@
 package librarysys.dao;
 
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import librarysys.dao.interfaces.AbstractDAO;
+import librarysys.entities.BaseEntity;
 
 @Repository
 public abstract class AbstractDAOImpl<T , ID> implements AbstractDAO<T, ID>{
 	
+//	protected Class<T> type;
+//	
+//	public AbstractDAOImpl(){}
+//	
+//	public AbstractDAOImpl(Class<T> type){
+//		this.type = type;
+//	}
+	
 	@Autowired
     private SessionFactory sessionFactory;
+	
+	@SuppressWarnings("unchecked")
+	public T getByPrimaryKey(Integer key) {
+		return (T) getCurrentSession().load(BaseEntity.class, key);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> getAll() {
+		return (List<T>) getCurrentSession().createCriteria(BaseEntity.class).list();
+	}
 
 	public void save(T entity) {
 		sessionFactory.getCurrentSession().saveOrUpdate(entity);
